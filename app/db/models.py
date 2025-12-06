9d3fefb077894f4e5c612fb3685a27a4d53778c0
fahad <fahadkhanf715@gmail.com>
2025-12-06 23:44:41 +0530

    updated the models and prediction schema

--- app/db/models.py
+++ app/db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Float
 from sqlalchemy.orm import relationship
 from .database import Base
 from datetime import datetime
@@ -33,3 +33,51 @@ class passwordResetToken(Base):
     expires_at = Column(DateTime, nullable=False)
 
     user = relationship("User")
+
+class UserProfile(Base):
+    __tablename__ = "user_profile"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), unique=True)
+
+    gender = Column(String, nullable=True)
+    height_m = Column(Float, nullable=True)
+    weight_kg = Column(Float, nullable=True)
+    bmi = Column(Float, nullable=True)
+
+    created_at = Column(DateTime, default=datetime.utcnow)
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    user = relationship("User", back_populates="profile")
+
+
+class UserHealthProfile(Base):
+    __tablename__ = "user_health_profiles"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), unique=True)
+
+    # Core Info
+    gender = Column(String, nullable=True)
+    age = Column(Integer, nullable=True)
+    height_m = Column(Float, nullable=True)
+    weight_kg = Column(Float, nullable=True)
+    bmi = Column(Float, nullable=True)
+
+    # Lifestyle & Habits
+    family_overweight_history = Column(String, nullable=True)
+    high_calorie_food = Column(String, nullable=True)
+    vegetable_intake_freq = Column(Integer, nullable=True)
+    main_meals_per_day = Column(Integer, nullable=True)
+    snack_frequency = Column(String, nullable=True)
+    smokes = Column(String, nullable=True)
+    water_intake_liters = Column(Float, nullable=True)
+    calorie_tracking = Column(String, nullable=True)
+    physical_activity_hours = Column(Float, nullable=True)
+    screentime_hours = Column(Float, nullable=True)
+    alcohol_consumption = Column(String, nullable=True)
+    travel_mode = Column(String, nullable=True)
+
+    updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    user = relationship("User", back_populates="health_profile")
+
--- app/schemas/predict.py
+++ app/schemas/predict.py
@@ -1,31 +1,38 @@
 from pydantic import BaseModel
+from typing import Literal
 
 class PredictionRequest(BaseModel):
-    gender: str = "Female"
+
+    gender: Literal["Male", "Female"] = "Female"
+
     age: float = 25.0
     height_m: float = 1.65
     weight_kg: float = 65.0
-    
-    family_overweight_history: str = "no"
-    high_calorie_food: str = "no"
-    
+
+    family_overweight_history: Literal["yes", "no"] = "no"
+    high_calorie_food: Literal["yes", "no"] = "no"
+
     vegetable_intake_freq: float = 2.0
     main_meals_per_day: float = 3.0
-    
-    snack_frequency: str = "Sometimes"  # matches dataset
-    smokes: str = "no"
-    
+
+    snack_frequency: Literal["no", "Sometimes", "Frequently"] = "Sometimes"
+    smokes: Literal["yes", "no"] = "no"
+
     water_intake_liters: float = 2.0
-    calorie_tracking: str = "no"
-    
+    calorie_tracking: Literal["yes", "no"] = "no"
+
     physical_activity_hours: float = 0.5
     screentime_hours: float = 4.0
-    
-    alcohol_consumption: str = "no"
-    travel_mode: str = "Public_Transportation"
-    
-    
 
+    alcohol_consumption: Literal["no", "Sometimes", "Frequently"] = "no"
+
+    travel_mode: Literal[
+        "Car",
+        "Walking",
+        "Bike",
+        "Motorbike",
+        "Public_Transportation"
+    ] = "Public_Transportation"
 
 class PredictionResponse(BaseModel):
-    prediction: str
+    obesity_level:str
